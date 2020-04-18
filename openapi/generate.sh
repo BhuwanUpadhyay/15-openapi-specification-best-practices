0b4b5d5f43f274c17e63f81a69f67a70796ef2fb
Bhuwan Upadhyay <bhuwan.upadhyay@clusus.com>
2020-04-18 17:47:37 +0545

    [WIP]

--- openapi/generate.sh
+++ openapi/generate.sh
@@ -1,3 +1,3 @@
 openapi-generator batch openapi/generators/spring-boot.yaml
 cd out/spring-boot
-mvn com.coveo:fmt-maven-plugin:format install
+mvn com.coveo:fmt-maven-plugin:format source:jar-no-fork javadoc:jar install
